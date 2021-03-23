2f528e9f76181c976f5c721a4d8f3df0b9c61c0d
kratuvwxyz <sai@desaign.me>
2021-03-23 11:40:23 -0500

    log changed

--- script.js
+++ script.js
@@ -1,173 +1,86 @@
-// display information
 letsPlay = () => {
-    $('#intro-text').fadeOut(2000);
-    $('.hidePlayerOne').fadeIn(2000);
-}
-
+  $("#intro-text").fadeOut(2e3), $(".hidePlayerOne").fadeIn(2e3);
+};
 let player1 = "",
-    player2 = "";
-    
-function hideThem (x, y, z, w) {
-
-	if(x == ""){
-		$('#'+y).toggleClass('shake');
-	} else {
-		$("."+z).hide();
-		$("."+w).fadeIn(2000);
-	}
-	
-	
+  player2 = "";
+function hideThem(e, r, o, t) {
+  "" == e ? $("#" + r).toggleClass("shake") : ($("." + o).hide(), $("." + t).fadeIn(2e3));
 }
-    
-$(document).on('click', '.nextPlayerTwo', () => {
-	player1 = $('#playerOne').val();
-	hideThem (player1, "playerOne", "hidePlayerOne", "hidePlayerTwo");    
-});
-
-
-
-$('.nextShowGame').click(() => {
-	
-	player2 = $('#playerTwo').val();
-	hideThem (player2, "playerTwo", "hidePlayerTwo", "hideGame"); 
-	gameUp();
-});
-
-// before we start, let's setup!
-
-let sign = 'X',
-    disp = $('#player'),
-    isko = "",
-    x = 0,
-    o = 0;
-console.log(player1);
-console.log(player2);
-
+$(document).on("click", ".nextPlayerTwo", () => {
+  (player1 = $("#playerOne").val()), hideThem(player1, "playerOne", "hidePlayerOne", "hidePlayerTwo");
+}),
+  $(".nextShowGame").click(() => {
+    (player2 = $("#playerTwo").val()), hideThem(player2, "playerTwo", "hidePlayerTwo", "hideGame"), gameUp();
+  });
+let sign = "X",
+  disp = $("#player"),
+  isko = "",
+  x = 0,
+  o = 0;
 gameUp = () => {
-    
-    // else keep the name in the prompt
-    $('.player1Name').text(player1).addClass('textTransform');
-    $('.player2Name').text(player2).addClass('textTransform');
-    disp.html(`Let's Play! <span style="color:#fff;" class="textTransform">${sign == 'X' ? player1 : player2}</span>, it's your turn!`)
-    
-    // main function 
-    printx = (number) => {
-        isko = $('#r' + number);
-        // !(not) let throw an error if user want to add on something alrady typed
-        if (!$(isko).html()) {
-            $(isko).text(sign).css('background-color', 'rgb(230, 230, 230)').css('color', 'black').css('cursor', 'grab');
-            winner();//check if you win or not
-            checksign();//change user
-            $(disp).html(`<center><span style="color:#fff;" class="textTransform"> ${sign == 'X' ? player1 : player2}</span>, it's your turn to play.</center>`);
-        } else {
-            throw 'error';
-        }
-    }
-    
-    // change 'X' or 'O'
-    checksign = () => {
-        sign == 'X' ? sign = "O" : sign = "X";
-    }
-    
-    // return id value
-    getbox = (number) => {
-        return $('#r' + number).text();
-    }
-    
-    // return * value
-    starbox = (number) => {
-        return $('#r' + number).text('•').css('color', '#2a2a2a');
-    }
-    
-    // if all are matching then return true (win) else false
-    checkmove = (a, b, c, m) => {
-        if (getbox(a) == m && getbox(b) == m && getbox(c) == m) {
-            $('#r' + a).css('background-color', '#dc3545').css('color', '#ffffff');
-            $('#r' + b).css('background-color', '#dc3545').css('color', '#ffffff');
-            $('#r' + c).css('background-color', '#dc3545').css('color', '#ffffff');
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    // reset after game done
-    reset = () => {
-        $('#x').text(x);
-        $('#o').text(o);
-        isko = "";
-        $(disp).html(`<center>Winner plays first! <span style="color:#fff;" class="textTransform"> ${sign == 'X' ? player1 : player2}</span>, Your turn!</center>`)
-        for (let i = 1; i < 10; i++) {
-            $('#r' + i).html("").css('background-color', 'rgb(187, 186, 186)').css('cursor', 'grabbing');
-        }
-    }
-    
-    // check winner in every move
-    winner = () => {
-        if (
-            checkmove(1, 2, 3, sign) ||
-            checkmove(4, 5, 6, sign) ||
-            checkmove(7, 8, 9, sign) ||
-            checkmove(1, 4, 7, sign) ||
-            checkmove(2, 5, 8, sign) ||
-            checkmove(3, 6, 9, sign) ||
-            checkmove(1, 5, 9, sign) ||
-            checkmove(3, 5, 7, sign)) {
-            $(disp).html(`<center>Winner winner <span style='color: #fff;' class="textTransform"> ${sign == 'X' ? player1 : player2}</span> is the winner!</center>`);
-            
-            // score
-            sign == 'X' ? x++ : o++;
-            
-			// if it's empty then fill with some stuff so that user can't add more
-			if(getbox(1) == "") {starbox(1)}
-			if(getbox(2) == "") {starbox(2)}
-			if(getbox(3) == "") {starbox(3)}
-			if(getbox(4) == "") {starbox(4)}
-			if(getbox(5) == "") {starbox(5)}
-			if(getbox(6) == "") {starbox(6)}
-			if(getbox(7) == "") {starbox(7)}
-			if(getbox(8) == "") {starbox(8)}
-			if(getbox(9) == "") {starbox(9)}
-            
-            // adding set time out for 3 sec. to see the final result
-            setTimeout(reset, 3000);
-            
-            // end of the game
-            throw 'game over';
-            
-        } else {
-            if (getbox(1) != "" &&
-                getbox(2) != "" &&
-                getbox(3) != "" &&
-                getbox(4) != "" &&
-                getbox(5) != "" &&
-                getbox(6) != "" &&
-                getbox(7) != "" &&
-                getbox(8) != "" &&
-                getbox(9) != "") {
-                $(disp).html(`<center>It's a tie. Play again</center>`);
-                setTimeout(reset, 3000);
-                throw 'it is a tie'
-            }
-        }
-    }
-
+  $(".player1Name").text(player1).addClass("textTransform"),
+    $(".player2Name").text(player2).addClass("textTransform"),
+    disp.html(`Let's Play! <span style="color:#fff;" class="textTransform">${"X" == sign ? player1 : player2}</span>, it's your turn!`),
+    (printx = (e) => {
+      if (((isko = $("#r" + e)), $(isko).html())) throw "error";
+      $(isko).text(sign).css("background-color", "rgb(230, 230, 230)").css("color", "black").css("cursor", "grab"), winner(), checksign(), $(disp).html(`<center><span style="color:#fff;" class="textTransform"> ${"X" == sign ? player1 : player2}</span>, it's your turn to play.</center>`);
+    }),
+    (checksign = () => {
+      sign = "X" == sign ? "O" : "X";
+    }),
+    (getbox = (e) => $("#r" + e).text()),
+    (starbox = (e) =>
+      $("#r" + e)
+        .text("•")
+        .css("color", "#2a2a2a")),
+    (checkmove = (e, r, o, t) =>
+      getbox(e) == t &&
+      getbox(r) == t &&
+      getbox(o) == t &&
+      ($("#r" + e)
+        .css("background-color", "#dc3545")
+        .css("color", "#ffffff"),
+      $("#r" + r)
+        .css("background-color", "#dc3545")
+        .css("color", "#ffffff"),
+      $("#r" + o)
+        .css("background-color", "#dc3545")
+        .css("color", "#ffffff"),
+      !0)),
+    (reset = () => {
+      $("#x").text(x), $("#o").text(o), (isko = ""), $(disp).html(`<center>Winner plays first! <span style="color:#fff;" class="textTransform"> ${"X" == sign ? player1 : player2}</span>, Your turn!</center>`);
+      for (let e = 1; e < 10; e++)
+        $("#r" + e)
+          .html("")
+          .css("background-color", "rgb(187, 186, 186)")
+          .css("cursor", "grabbing");
+    }),
+    (winner = () => {
+      if (checkmove(1, 2, 3, sign) || checkmove(4, 5, 6, sign) || checkmove(7, 8, 9, sign) || checkmove(1, 4, 7, sign) || checkmove(2, 5, 8, sign) || checkmove(3, 6, 9, sign) || checkmove(1, 5, 9, sign) || checkmove(3, 5, 7, sign)) throw ($(disp).html(`<center>Winner winner <span style='color: #fff;' class="textTransform"> ${"X" == sign ? player1 : player2}</span> is the winner!</center>`), "X" == sign ? x++ : o++, "" == getbox(1) && starbox(1), "" == getbox(2) && starbox(2), "" == getbox(3) && starbox(3), "" == getbox(4) && starbox(4), "" == getbox(5) && starbox(5), "" == getbox(6) && starbox(6), "" == getbox(7) && starbox(7), "" == getbox(8) && starbox(8), "" == getbox(9) && starbox(9), setTimeout(reset, 3e3), "game over");
+      if ("" != getbox(1) && "" != getbox(2) && "" != getbox(3) && "" != getbox(4) && "" != getbox(5) && "" != getbox(6) && "" != getbox(7) && "" != getbox(8) && "" != getbox(9)) throw ($(disp).html("<center>It's a tie. Play again</center>"), setTimeout(reset, 3e3), "it is a tie");
+    });
 };
 
-
-//     ╥╥  DESIGNED AND DEVELOPED BY...
-//     ║║ 
-//     ║║ ╓╓──╖╖ ╓╓──── ────╖╖ ╓╖ ╓╓──╖╖ ╓╓──╖╖ TM
-//     ║║ ║║  ║║ ║║         ║║ ╙╜ ║║  ║║ ║║  ║║
-// ╓╓──╫╫ ╫╫──╜╜ ╙╙──╖╖ ╓╓──╫╫ ╖╖ ║║  ║║ ║║  ║║
-// ║║  ║║ ║║         ║║ ║║  ║║ ║║ ║║  ║║ ║║  ║║
-// ║║  ║║ ║║         ║║ ║║  ║║ ║║ ║║  ║║ ║║  ║║
-// ║║  ║║ ╙╙──── ────╜╜ ╙╙──╜╜ ║║ ╙╙──╫╫ ╜╜  ║║
-// ║║  ║║  .STUDIO             ║║     ║║     ╙╙ LLC
-// ╙╙──╜╜  SINCE TWENTYELEVEN  ╙╙─ ───╜╜
-//     LLC © HTTPS://DESAIGN.APP
-//     ALL RIGHTS RESERVED
-//     --
-//     Call: 1-860-DESAIGN
-//     Mail: MEET@DESAIGN.STUDIO
-//     Post: PO BOX 200001, AUSTIN TX 78720
+/*  
+        ╥╥  DESIGNED AND DEVELOPED BY...
+        ║║ 
+        ║║ ╓╓──╖╖ ╓╓──── ────╖╖ ╓╖ ╓╓──╖╖ ╓╓──╖╖ TM
+        ║║ ║║  ║║ ║║         ║║ ╙╜ ║║  ║║ ║║  ║║
+    ╓╓──╫╫ ╫╫──╜╜ ╙╙──╖╖ ╓╓──╫╫ ╖╖ ║║  ║║ ║║  ║║
+    ║║  ║║ ║║         ║║ ║║  ║║ ║║ ║║  ║║ ║║  ║║
+    ║║  ║║ ║║         ║║ ║║  ║║ ║║ ║║  ║║ ║║  ║║
+    ║║  ║║ ╙╙──── ────╜╜ ╙╙──╜╜ ║║ ╙╙──╫╫ ╜╜  ║║
+    ║║  ║║  HTTPS://DESAIGN.APP ║║     ║║     ╙╙ LLC
+    ╙╙──╜╜  SINCE TWENTYELEVEN  ╙╙─ ───╜╜
+            Copyright © 2021
+            ALL RIGHTS RESERVED
+            --
+            Call: 1-860-DESAIGN
+            Mail: MEET@DESAIGN.STUDIO
+            Post: PO BOX 200001, AUSTIN TX 78720
+            --
+            Book an appointment with us at
+            https://calendly.com/desaignstudio
+            --
+            Follow us: #desaignstudio
+*/
